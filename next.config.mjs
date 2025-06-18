ea13f0bb9bb6bf3ecfad3fe497706ffb76824fd2
kolay odeme <kenbladex1@gmail.com>
2025-06-18 10:32:22 +0300

    Build yapılandırması basitleştirildi

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   trailingSlash: true,
-  basePath: '',
-  assetPrefix: '',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true
   }
